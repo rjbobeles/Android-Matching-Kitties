74a092deba3121e944f0f56d7adf2d990ecc828b
Gel Gamoras <gelgamoras@gmail.com>
2021-04-11 07:11:03 +0800

    Added binding to authors and level selection activities

--- app/src/main/java/ph/edu/benilde/matchingkitties/AuthorsActivity.kt
+++ app/src/main/java/ph/edu/benilde/matchingkitties/AuthorsActivity.kt
@@ -2,17 +2,22 @@ package ph.edu.benilde.matchingkitties
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ph.edu.benilde.matchingkitties.databinding.ActivityAuthorsBinding
 
 class AuthorsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAuthorsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAuthorsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Remove title bar
-        setContentView(R.layout.activity_authors)
         if (supportActionBar != null) {
             this.supportActionBar?.hide();
         }
 
-        setContentView(R.layout.activity_authors)
+
     }
 }
--- app/src/main/java/ph/edu/benilde/matchingkitties/LevelSelectionActivity.kt
+++ app/src/main/java/ph/edu/benilde/matchingkitties/LevelSelectionActivity.kt
@@ -2,17 +2,27 @@ package ph.edu.benilde.matchingkitties
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ph.edu.benilde.matchingkitties.databinding.ActivityLevelSelectionBinding
 
 class LevelSelectionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLevelSelectionBinding
+
+    private val btnClose by lazy { binding.btnClose }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLevelSelectionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Remove title bar
-        setContentView(R.layout.activity_level_selection)
         if (supportActionBar != null) {
             this.supportActionBar?.hide();
         }
 
-        setContentView(R.layout.activity_level_selection)
+        btnClose.setOnClickListener{
+            finish()
+        }
+
     }
 }
