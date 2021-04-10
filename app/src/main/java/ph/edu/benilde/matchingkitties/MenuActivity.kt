02cdfb719834d951f69a71989765909243d17b70
Gel Gamoras <gelgamoras@gmail.com>
2021-04-11 07:10:16 +0800

    added styles to menu activity + style fixes

--- app/src/main/java/ph/edu/benilde/matchingkitties/MenuActivity.kt
+++ app/src/main/java/ph/edu/benilde/matchingkitties/MenuActivity.kt
@@ -1,18 +1,29 @@
 package ph.edu.benilde.matchingkitties
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import ph.edu.benilde.matchingkitties.databinding.ActivityMenuBinding
 import android.os.Bundle
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding
+
+    private val btnCasual by lazy { binding.btnCasual }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Remove title bar
-        setContentView(R.layout.activity_menu)
         if (supportActionBar != null) {
             this.supportActionBar?.hide();
         }
 
-        setContentView(R.layout.activity_menu)
+        btnCasual.setOnClickListener {
+            val intent = Intent(this, LevelSelectionActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
